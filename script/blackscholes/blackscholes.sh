db4ae0498f7820e71cbe1bfa8f936ba3bb8139ef
szgxzdk <siyuanxiao1337@qq.com>
2018-02-21 16:14:55 +0800

    CMP all trace version with blackscholes from PARSEC

--- script/blackscholes/blackscholes.sh
+++ script/blackscholes/blackscholes.sh
@@ -2,8 +2,8 @@ rm -f ./trace/flow_blackscholes_xy_org.log
 rm -f ./trace/router_blackschole_xy_org.log
 
 ../../cmp_org_huq_all_trace/sim-outorder \
-        -redir:sim ./log/test_blackschole_xy_org_106.txt \
-        -redir:dump ./log/test_blackschole_xy_org_106.out \
+        -redir:sim ./log/test_blackschole_xy_org.txt \
+        -redir:dump ./log/test_blackschole_xy_org.out \
         -flow_trace ./trace/flow_blackschole_xy_org.log \
         -router_trace ./trace/router_blackschole_xy_org.log \
         -cpu:trace ./trace/cpu_blackschole_xy_org.log \
